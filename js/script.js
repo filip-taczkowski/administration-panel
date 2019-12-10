6823db968ef02a850021fb89a5dce3c227c420ee
Filip Taczkowski <filip.taczkowski@gmail.com>
2019-12-10 19:19:47 +0100

    Add js for nav links

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 {
+    /* Toggle hamburger */
+
     const hamburgerInput = document.querySelector('.toggle-hamburger');
     const sideNav = document.querySelector('.nav--side');
     const content = document.querySelector('.content');
@@ -8,4 +10,47 @@
         content.classList.toggle('--hamburger-toggled-content' , hamburgerInput.checked);
         console.log(content);
     });
+
+    /* Side Navigation */
+
+    const initSection = {};
+
+    initSection.general = document.getElementById('general');
+    initSection.links = document.getElementById('links');
+
+    const navLinks = document.querySelectorAll('.nav__link');
+    const sections = document.querySelectorAll('.container');
+
+    function showSection() {
+        for (let navLink of navLinks) {
+            navLink.addEventListener('click', () => {
+                const sectionToActivate = navLink.getAttribute('href').replace('#', '');
+                console.log(sectionToActivate);
+                
+                for (let section of sections) {
+                    const sectionId = section.getAttribute('id');
+                    if (sectionId == sectionToActivate) {
+                        section.classList.add('active');
+                    } else {
+                        section.classList.remove('active');
+                    }
+                }
+            })
+        }
+    }
+
+    function initSideNav() {
+        if (!initSection.general.classList.contains('active')) {
+            initSection.general.classList.add('active')
+        }
+
+        if (!initSection.links.classList.contains('active')) {
+            initSection.links.classList.add('active')
+        }
+
+        
+    }
+
+    initSideNav();
+    showSection();
 }
